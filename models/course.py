3884d6e5b88b0fbcf7ee0648aaec33b47417d6ee
DeibyLopez1 <deiby.lopez@unah.hn>
2022-03-01 21:19:34 +0100

    busqueda de cursos

--- models/course.py
+++ models/course.py
@@ -10,3 +10,8 @@ class Course(models.Model):
      name = fields.Char(String='Course')
      title = fields.Char(String='Title')
      description = fields.Text(String='Description')
+
+@api.model
+def _name_description_search(self, name='',description='', args=None, operator='ilike', limit=100, 
+name_get_uid=None, description_get_uid=None):
+ return self._search(args, limit=limit, access_rights_uid=name_get_uid,access_rights_uid=description_get_uid)
