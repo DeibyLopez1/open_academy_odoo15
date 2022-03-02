10121c55d9eb91429bdd2cccce7a045d4016a310
DeibyLopez1 <deiby.lopez@unah.hn>
2022-03-02 14:58:07 +0100

    Many2one

--- models/sesion.py
+++ models/sesion.py
@@ -1,3 +1,4 @@
+import string
 from odoo import models, fields, api
 name = fields.Char(string='Title')
 description = fields.Text()
@@ -10,3 +11,6 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    instructor_id=fields.Many2many('res.partner', string="Instuctor")
+    course_id=fields.Many2many('open_academy.course', ondelete='cascade', string="course",required=True)
+    
